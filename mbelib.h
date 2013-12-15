2656ec2202e15468f92dca0c226919166928c52e
balr0g <balrog032@gmail.com>
2013-12-14 19:05:53 -0500

    Initial DSTAR AMBE implementation

--- mbelib.h
+++ mbelib.h
@@ -46,6 +46,18 @@ int mbe_hamming1511 (char *in, char *out);
 int mbe_7100x4400hamming1511 (char *in, char *out);
 
 /*
+ * Prototypes from ambe3600x2400.c
+ */
+int mbe_eccAmbe3600x2400C0 (char ambe_fr[4][24]);
+int mbe_eccAmbe3600x2400Data (char ambe_fr[4][24], char *ambe_d);
+int mbe_decodeAmbe2400Parms (char *ambe_d, mbe_parms * cur_mp, mbe_parms * prev_mp);
+void mbe_demodulateAmbe3600x2400Data (char ambe_fr[4][24]);
+void mbe_processAmbe2400Dataf (float *aout_buf, int *errs, int *errs2, char *err_str, char ambe_d[49], mbe_parms * cur_mp, mbe_parms * prev_mp, mbe_parms * prev_mp_enhanced, int uvquality);
+void mbe_processAmbe2400Data (short *aout_buf, int *errs, int *errs2, char *err_str, char ambe_d[49], mbe_parms * cur_mp, mbe_parms * prev_mp, mbe_parms * prev_mp_enhanced, int uvquality);
+void mbe_processAmbe3600x2400Framef (float *aout_buf, int *errs, int *errs2, char *err_str, char ambe_fr[4][24], char ambe_d[49], mbe_parms * cur_mp, mbe_parms * prev_mp, mbe_parms * prev_mp_enhanced, int uvquality);
+void mbe_processAmbe3600x2400Frame (short *aout_buf, int *errs, int *errs2, char *err_str, char ambe_fr[4][24], char ambe_d[49], mbe_parms * cur_mp, mbe_parms * prev_mp, mbe_parms * prev_mp_enhanced, int uvquality);
+
+/*
  * Prototypes from ambe3600x2450.c
  */
 int mbe_eccAmbe3600x2450C0 (char ambe_fr[4][24]);
